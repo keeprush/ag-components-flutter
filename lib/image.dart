6704228f912ffd9a28451ac00ab2a76aee4473f4
keeprush <dlagudwh4@naver.com>
2022-01-04 15:37:49 +0900

    FEAT Button 1/2

---
+++ lib/image.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ui_ux/tab_bar.dart';
+
+class ImageClass extends StatefulWidget {
+  const ImageClass({Key? key}) : super(key: key);
+
+  @override
+  ImageState createState() => ImageState();
+}
+
+class ImageState extends State<ImageClass> {
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      appBar: appBarStandard(context),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            textButton(context),
+            elevatedButton(context),
+            outLineButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget textButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: const Text("Text BUTTON"),
+    );
+  }
+
+  Widget elevatedButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text("Elevated BUTTON"),
+    );
+  }
+
+  Widget outLineButton(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      child: const Text("Outlined BUTTON"),
+    );
+  }
+
+  PreferredSizeWidget appBarStandard(context) {
+    return AppBar(
+      title: const Text(
+        'Button',
+      ),
+      backgroundColor: Colors.blueAccent,
+      automaticallyImplyLeading: false,
+      leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
+      actions: [
+        IconButton(
+            icon: const Icon(
+              Icons.arrow_forward,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.push<Widget>(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TabBarClass(),
+                ),
+              );
+            }),
+      ],
+      elevation: 0,
+    );
+  }
+}
